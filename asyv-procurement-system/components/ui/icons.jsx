f1cd68eb170ffa61523e1f838e5a5f295614b716
Willy MUTESA <148581641+Mutesawilly@users.noreply.github.com>
2025-12-05 15:26:08 +0200

    Delete asyv-procurement-system directory

--- asyv-procurement-system/components/ui/icons.jsx
+++
@@ -1,9 +0,0 @@
-import { FaGithub } from "react-icons/fa";
-import { FcGoogle } from "react-icons/fc";
-import { FaSpinner } from "react-icons/fa";
-
-export const Icons = {
-  gitHub: (props) => <FaGithub {...props} />,
-  google: (props) => <FcGoogle {...props} />,
-  spinner: (props) => <FaSpinner {...props} />,
-};
